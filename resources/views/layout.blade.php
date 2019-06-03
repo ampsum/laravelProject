ae0be31cd8b62ccdeedc042722020d8a196bba25
Alexander <alex.maktabi@icloud.com>
2019-06-03 10:47:00 +0200

    merge

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,21 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>@yield('title','Projekt')</title>
 </head>
 <body>
     <header>
-        <ul>
-            <li><a href="/">Hem</a></li>
-            <li><a href="/blog">Bloggen</a></li>
-            <li><a href="/events">Evenemang</a></li>
-            <li><a href="/counter">Räkna utsläpp</a></li>
-            <li><a href="/profile">Mina sidor</a></li>
-            <li><a href="/login">Logga in</a></li>
-        </ul>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="/">Hem</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/blog">Bloggen</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/events">Evenemang</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/counter">Räkna utsläpp</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/profile">Mina sidor</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/login">Logga in</a></li>
+                </ul>
+            </div>
+        </nav>
     </header>
     <div id="wrap">
-        @yield('content');
+        @yield('content')
     </div>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
