b15a70283705ebc0aa6d7b81175ca5f5d19fa610
Alexander <alex.maktabi@icloud.com>
2019-06-04 09:15:35 +0200

    added counter.blade & support for custom css&js

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+var $ = jQuery;
+$(document).ready(function () {
+    // $('.counter-div form').submit((e) => {
+    //     e.preventDefault;
+    // })
+    // $('#count').click(() => {
+    //     $('.counter-div').addClass('active');
+    // })
+    // $('#close').click(() => {
+    //     $('.counter-div').removeClass('active');
+    // })
+});
