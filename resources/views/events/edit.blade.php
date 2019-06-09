6a2e728cebff68f39538d1d752ecdc5cf480e3f3
Alexander <alex.maktabi@icloud.com>
2019-06-09 16:01:59 +0200

    updates

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -1,23 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
+<h2 class="form-h2">Redigera {{$event->title}}</h2>
 <form action="/events/{{$event->id}}" method="POST" enctype="multipart/form-data">
         {{method_field('PATCH')}}
         {{csrf_field()}}
         <input type="hidden" name="event-set" value="1">
-        <input type="text" name="title" placeholder="Title" value="{{$event->title}}">
-        <input type="text" name="address" placeholder="Adress" value="{{$event->address}}">
-        <input type="date" name="date" placeholder="Datum" value="{{$event->date}}">
-        <input type="file" name="cover" placeholder="Coverbild" value="{{$event->cover}}">
-        <textarea name="content" cols="30" rows="10">{{$event->content}}</textarea>
-        <input type="hidden" name="lat">
-        <input type="hidden" name="long">
-        <input type="submit" name="submit" value="Uppdatera event">
+        <div class="form-group row">
+            <div class="col-md-6">
+                <input class="form-control" type="text" name="title" placeholder="Title" value="{{$event->title}}">
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-md-6">
+                <input class="form-control" type="text" name="address" placeholder="Address" value="{{$event->address}}">
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-md-6">
+                <input class="form-control" type="date" name="date" placeholder="Datum" value="{{$event->date}}">
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-md-6">
+                <input class="file-input" type="file" name="cover" placeholder="Coverbild" value="{{$event->cover}}">
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-md-6">
+                <label for="content">Content</label> 
+            <textarea id="content" class="form-control" name="content" cols="30" rows="10">{{$event->content}}</textarea>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-md-6">
+                <input class="btn btn-primary" type="submit" name="submit" value="Uppdatera event">
+            </div>
+        </div>
     </form>
 
     <form action="/events/{{$event->id}}" method="post">
     {{csrf_field()}}
     {{method_field('DELETE')}}
-    <input type="submit" value="Ta bort event">
+     <div class="form-group row">
+         <div class="col-md-6">
+            <input class="btn btn-primary" type="submit" value="Ta bort event">
+         </div>   
+     </div>
     </form>
 @endsection
